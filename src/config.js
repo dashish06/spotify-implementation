db4f02cfb39da060ceea935ed9f2d976e4319f20
dashish06 <ashishdwivedi06@gmail.com>
2021-05-22 19:16:15 +0530

    initial commit for existing code

---
+++ src/config.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line
+export default {
+  api: {
+    baseUrl: 'https://api.spotify.com/v1',
+    authUrl: 'https://accounts.spotify.com/api/token',
+    clientId: 'ab2d49a207cd43ada83ed595cbb0f0f7',
+    clientSecret: '19bfbcc567e94795b6730d72ff83b0ff'
+  }
+}
